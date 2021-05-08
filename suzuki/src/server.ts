c5af5b8822cfbb86af75804b0ddb0cece9e65359
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-05-08 04:10:00 +0200

    Errors integrated

---
+++ suzuki/src/server.ts
@@ -0,0 +1,26 @@
+import express, { json, urlencoded } from "express";
+import cors from "cors";
+import { api } from "./api";
+import { Errors } from "./errors/errorHandler";
+
+export const server = async (port: number) => {
+
+    try {
+
+        const app = express();
+        
+        app.use(cors(), json(), urlencoded({extended: true}));
+
+        app.use(api());
+        
+        app.listen(port, () => {
+            console.log('Suzuki on port', port);
+        });
+
+    } catch(error) {
+
+        Errors.log(error, './server.ts server initialization');
+
+    }
+}
+
