854cb4444af8240806cc6d5c35ebe45c34663dd0
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-05-09 04:21:00 +0200

    login system done

--- chevrolet/src/database.ts
+++ chevrolet/src/database.ts
@@ -1,8 +1,49 @@
 
-interface User {
+const generateId = (length: number) => {
+    let id: string = '';
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ1234567890';
+    for(let i = 0; i < length; i++) id += chars.charAt(Math.floor(Math.random() * chars.length));
+    return id;
+};
+
+interface UserOut {
+    _id: string,
     name: string,
     address: string,
     email: string,
     tel: string,
     password: string
+}
+
+interface UserIn {
+    name: string,
+    address: string,
+    email: string,
+    tel: string,
+    password: string
+}
+
+export class User {
+
+    private static table: UserOut[] = [];
+
+    public static insert = (user: UserIn) => {
+        const id = generateId(16);
+        User.table.push({
+            _id: id,
+            ...user
+        });
+        return id;
+    }
+
+    public static find = (test: (user: UserOut) => boolean) => {
+        for(let i in User.table) if(test(User.table[i])) return User.table[i];
+    }
+    
+    public static get = (id: string) => {
+        for(let i in User.table) if(User.table[i]._id === id) return User.table[i];
+    }
+
+
+
 }
