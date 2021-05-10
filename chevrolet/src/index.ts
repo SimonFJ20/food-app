67f6221e8e9316c22e9edee5ae9c227c6ff5d850
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-05-10 09:25:01 +0200

    dev router

--- chevrolet/src/index.ts
+++ chevrolet/src/index.ts
@@ -1,5 +1,7 @@
 import './body.scss';
 import { listScreen } from './listScreen/listScreen';
+import { loginScreen } from './loginScreen/loginScreen';
+import { settingsScreen } from './settingsScreen/settingsScreen';
 
 document.title = 'FeedMe App Draft';
 document.head.innerHTML += /*html*/`
@@ -14,5 +16,27 @@ document.head.innerHTML += /*html*/`
 `;
 
 
-listScreen();
+const params = new URLSearchParams(window.location.search);
+
+if(params.has('p')) {
+    const page = params.get('p');
+    
+    switch(page) {
+        case 'list':
+            listScreen();
+            break;
+        case 'login':
+            loginScreen();
+            break;
+        case 'settings':
+            settingsScreen();
+            break;
+        default:
+            listScreen();
+            break;
+    }
+    
+} else {
+    listScreen();
+}
 
