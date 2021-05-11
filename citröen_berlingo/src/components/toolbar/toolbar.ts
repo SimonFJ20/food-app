107e79a450eea5b185dfe415d20e0d636856c400
piet <42157469+camper0008@users.noreply.github.com>
2021-05-11 12:16:14 +0200

    added url parameters

--- citröen_berlingo/src/components/toolbar/toolbar.ts
+++ citröen_berlingo/src/components/toolbar/toolbar.ts
@@ -1,36 +1,66 @@
 import html from './toolbar.html';
 import './toolbar.scss';
 
-import { listScreen } from '../../listScreen/listScreen';
-import { settingsScreen } from '../../settingsScreen/settingsScreen';
-
 import settingsImage from './settings.png'
 import settingsSelectImage from './settings-select.png'
 import searchImage from './search.png'
 import searchSelectImage from './search-select.png'
-import deliveryImage from './delivery.png'
-import deliverySelectImage from './delivery-select.png'
+import deliveriesImage from './deliveries.png'
+import deliveriesSelectImage from './deliveries-select.png'
 
 export const toolbar = () => {
     let returnHtml = html;
+
+    const params = new URLSearchParams(window.location.search);
+
+    if(params.has('p')) {
+        const page = params.get('p');
+        
+        switch(page) {
+            case 'list':
+                returnHtml = returnHtml.replace(/{{searchImage}}/g, '{{searchSelectImage}}')
+                break;
+            case 'settings':
+                returnHtml = returnHtml.replace(/{{settingsImage}}/g, '{{settingsSelectImage}}')
+                break;
+            case 'deliveries':
+                returnHtml = returnHtml.replace(/{{deliveriesImage}}/g, '{{deliveriesSelectImage}}')
+                break;    
+            default:
+                returnHtml = returnHtml.replace(/{{searchImage}}/g, '{{searchSelectImage}}')
+                break;
+        }
+    } else {
+        returnHtml = returnHtml.replace(/{{searchImage}}/g, '{{searchSelectImage}}')
+    }
+
     returnHtml = returnHtml.replace(/{{settingsImage}}/g, settingsImage)
     .replace(/{{settingsSelectImage}}/g, settingsSelectImage)
     .replace(/{{searchImage}}/g, searchImage)
     .replace(/{{searchSelectImage}}/g, searchSelectImage)
-    .replace(/{{deliveryImage}}/g, deliveryImage)
-    .replace(/{{deliverySelectImage}}/g, deliverySelectImage);
+    .replace(/{{deliveriesImage}}/g, deliveriesImage)
+    .replace(/{{deliveriesSelectImage}}/g, deliveriesSelectImage);
 
     return returnHtml;
 }
 
 export const toolbarInit = () => {
+    const params = new URLSearchParams(window.location.search);
+
     document.getElementById('settingsToolbar')?.addEventListener('click', () => {
-        settingsScreen();
+        params.set('p', 'settings')
+
+        window.location.search = params.toString();
     })
     document.getElementById('searchToolbar')?.addEventListener('click', () => {
-        listScreen();
+        params.set('p', 'list')
+
+        window.location.search = params.toString();
+
     })
-    document.getElementById('deliveryToolbar')?.addEventListener('click', () => {
-        
+    document.getElementById('deliveriesToolbar')?.addEventListener('click', () => {
+        params.set('p', 'deliveries')
+
+        window.location.search = params.toString();
     })
 }
