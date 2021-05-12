02df825de79c4a605a112a87c79427995a4dd4ad
piet <42157469+camper0008@users.noreply.github.com>
2021-05-12 10:01:03 +0200

    react-native test

--- citröen_berlingo/react-native/App.js
+++ citröen_berlingo/react-native/App.js
@@ -5,9 +5,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-        <WebView
-          source={{ uri: 'https://infinite.red' }}
-          style={{ width: '100vw', height: '100vh' }}
+      <WebView
+        source={{ uri: 'https://infinite.red' }}
+        style={{ width: '100vw', height: '100vh' }}
       />
     </View>
   );
