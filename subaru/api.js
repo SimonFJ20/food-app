dfc28be1b9d3c14e3de2c6ca4d8d284d58fa5755
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-05-21 13:23:52 +0200

    Update api.js

--- subaru/api.js
+++ subaru/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const validators = require('./validators');
 const register = require('./api/register');
+const login = require('./api/login');
 
 
 module.exports = api = async () => {
@@ -9,7 +10,7 @@ module.exports = api = async () => {
     
     
     router.post('/register', validators.register, register);
-    
+    router.post('/login', validators.login, login);
     
     
     
