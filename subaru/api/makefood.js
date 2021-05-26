1ca4f8db0b7340623acf9baf5677c3e995422702
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-05-27 00:45:13 +0200

    asdasd

--- subaru/api/makefood.js
+++ subaru/api/makefood.js
@@ -1,11 +1,14 @@
 const { getDatabase } = require('../database')
+const bcrypt = require('bcrypt');
+
+const secret = '$2b$10$rdw7fHhbru0utWmZmwFxMujlmw/7KzAnVZLkMIpUWVArPfQJ4D/5u';
 
 module.exports = async (req, res) => {
 
     const database = getDatabase();
     const Foods = database.collection('foods');
 
-    if(req.body.secret !== 'ELJ9wcrQ68QAYQPP') {
+    if(await bcrypt.compare(req.body.secret, secret)) {
         res.status(400).json({success: false});
         return;
     };
