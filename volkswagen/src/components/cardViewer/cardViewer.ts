820e23060b8edb98a89266f2f3dc0b3472b5ba64
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-05-27 03:29:19 +0200

    asdasda

--- volkswagen/src/components/cardViewer/cardViewer.ts
+++ volkswagen/src/components/cardViewer/cardViewer.ts
@@ -7,5 +7,6 @@ export const cardViewer = (title: string, description: string, imageSrc: string,
     rhtml = rhtml.replace(/{{cardDescription}}/g, description);
     rhtml = rhtml.replace(/{{imageSrc}}/g, imageSrc);
     rhtml = rhtml.replace(/{{imageAlt}}/g, imageAlt);
+    rhtml = rhtml.replace(/{{cardPrice}}/g, price.toString());
     return rhtml;
 }
