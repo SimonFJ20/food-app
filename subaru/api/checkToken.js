be64bb7a6ae7035e8d2aade8f7a40afe4c6b65b2
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-05-27 10:27:12 +0200

    vlucf

---
+++ subaru/api/checkToken.js
@@ -0,0 +1,42 @@
+const { validationResult } = require('express-validator');
+const { getDatabase } = require('../database');
+const { check } = require('express-validator');
+const { ObjectId } = require('mongodb');
+
+module.exports = async (req, res) => {
+    try {
+        const database = getDatabase();
+        const Tokens = database.collection('tokens');
+        
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            res.status(400).json({
+                success: false,
+                response: 'invalid',
+                errors: errors.array()
+            });
+            return;
+        }
+        const token = req.body.token;
+        const existingToken = await Tokens.findOne({token: token});
+        if(!existingToken) {
+            res.status(400).json({
+                success: false,
+                response: 'unknown'
+            });
+            return;
+        }
+        
+        res.status(200).json({
+            success: true,
+            response: 'success'
+        });
+    } catch(error) {
+        console.error('/api/updateuser', error);
+        res.status(500).json({
+            success: false,
+            response: 'error',
+            error: error
+        });
+    }
+}
